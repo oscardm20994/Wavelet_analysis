fc2402b6a966744754a9b5b82af55ef592fa74da
Oscar Dimdore-Miles <32623897+oscardm20994@users.noreply.github.com>
2020-12-03 14:28:23 +0000

    Update waveletFunctions.py

--- waveletFunctions.py
+++ waveletFunctions.py
@@ -1,30 +1,8 @@
 from scipy.special._ufuncs import gammainc, gamma
 import numpy as np
 from scipy.optimize import fminbound
-__author__ = 'Evgeniya Predybaylo'
+__author__ = 'Oscar Dimdore-Miles'
 
-
-# Copyright (C) 1995-2004, Christopher Torrence and Gilbert P.Compo
-# Python version of the code is written by Evgeniya Predybaylo in 2014
-#
-#   This software may be used, copied, or redistributed as long as it is not
-#   sold and this copyright notice is reproduced on each copy made. This
-#   routine is provided as is without any express or implied warranties
-#   whatsoever.
-#
-# Notice: Please acknowledge the use of the above software in any publications:
-#    ``Wavelet software was provided by C. Torrence and G. Compo,
-#      and is available at URL: http://paos.colorado.edu/research/wavelets/''.
-#
-# Reference: Torrence, C. and G. P. Compo, 1998: A Practical Guide to
-#            Wavelet Analysis. <I>Bull. Amer. Meteor. Soc.</I>, 79, 61-78.
-#
-# Please send a copy of such publications to either C. Torrence or G. Compo:
-#  Dr. Christopher Torrence               Dr. Gilbert P. Compo
-#  Research Systems, Inc.                 Climate Diagnostics Center
-#  4990 Pearl East Circle                 325 Broadway R/CDC1
-#  Boulder, CO 80301, USA                 Boulder, CO 80305-3328, USA
-#  E-mail: chris[AT]rsinc[DOT]com         E-mail: compo[AT]colorado[DOT]edu
 #
 #-------------------------------------------------------------------------------------------------------------------
 
